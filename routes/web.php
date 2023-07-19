dbb7dd2779fe7d4bad9592964bff37ae068865d5
thallys97 <60667295+thallys97@users.noreply.github.com>
2023-07-19 12:39:08 -0500

    retirei outros comentários do web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UsersController;
-//use App\Http\Middleware\Autenticador;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::middleware('autenticador')->group(function() {
 
